28a3e0bc46b810a5ce1d51ce07ab1ca73959d867
Divyanshu-ui <dv62coolz@gmail.com>
2025-09-08 09:51:20 +0530

    DAY 6

--- day6.c
+++ day6.c
@@ -1,28 +1,17 @@
-#include<stdio.h>
+#include <stdio.h>
+int main() 
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
 
-int main(){
-
-    int  a, b, c, greatest;
-
-    printf("Enter the first number = ");
-    scanf("%d", &a);
-
-    printf("Enter the secound number = ");
-    scanf("%d", &b);
-
-    printf("Enter the third number = ");
-    scanf("%d", &c);
-
-    if(a >= b && a >= c) {
-        greatest = a;
-    } else if (b >= a && b >= c) {
-        greatest = b;
+    if (num >= 0) {
+        if (num == 0)
+            printf("Zero");
+        else
+            printf("Positive");
     } else {
-        greatest = c;
+        printf("Negative");
     }
-
-        printf("The greatest number is %d\n", greatest);
-
-        return 0;
-    
+    return 0;
 }
